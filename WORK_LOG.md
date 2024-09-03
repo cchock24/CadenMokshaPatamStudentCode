3e7c8d06996b2f58496d81338dea773f1da8049b
Caden <caden.chock@menloschool.org>
2024-09-02 21:17:49 -0700

    :/

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: YOUR NAME HERE
 
-| Date      |  Time  |                                         Update |
-|:----------|:------:|-----------------------------------------------:|
-| August 29 |  2:50-3:00| I made a fuction that finds the largest ladder |
-|           |        |                                                |
+| Date        |    Time     |                                                                                                                                                                                                                                                               Update |
+|:------------|:-----------:|---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| August 29   |  2:50-3:00  |                                                                                                                                                                                                                       I made a fuction that finds the largest ladder |
+| September 2 |  8:50-9:20  | I made a bunch of functions that I think will be helpful to solve this problems set. I have to current issues the fact that I can't use my non-static functions in the static instance of the initial method. 2 I have no clue how to go about finding the best path |
+| September 2 | 9:20 - 9:40 |                                                                                                                                                                                                                                                                      |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -14,12 +14,29 @@ public class MokshaPatam {
      * TODO: Complete this function, fewestMoves(), to return the minimum number of moves
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
+    // Steps
+    // 1) Find Closest Ladder
+    // 2) Find How Many Moves to Make it to Ladder (While Avoiding Snakes)
+    // 3) Check if Ladder is Worth it (Distance per Moves and what Ladders Missing)
+    // 4) Repeat Until End
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
-        // Find the Biggest Ladder
+        int position = 0;
+        while(position != 100){
+
+        }
+
+
+
+        return 0;
+    }
+
+    // Find the Biggest Ladder
+    public int findLargerLadder(int[][] ladders){
         int largest = 0;
+        int place = 0;
         for(int i = 0; i < ladders.length; i++){
             int challenger = 0;
-            int place = 0;
+
             for(int j = 0; j < ladders[i].length; j++){
                 // Calculate How Much Distance the Latter Covers
                 challenger += j==0 ? ladders[i][j] : -ladders[i][j];
@@ -29,6 +46,47 @@ public class MokshaPatam {
                 place = i;
             }
         }
-        return 0;
+        return place;
     }
+    // Find the Closest Ladder
+    public int findCloseLadder(int[][] ladders, int position) {
+        int place = 0;
+        boolean bool = true;
+        int counter = 0;
+        while (bool){
+            if (ladders[counter][0] > position) {
+                place = counter;
+                bool = false;
+            }
+            counter++;
+            if(counter > ladders.length){
+                bool = false;
+            }
+        }
+        return place;
+    }
+
+    // Find Distance to Next Ladder
+    public int distanceToLadder(int[][] ladders, int position, int ladder){
+        int distance = ladders[ladder][0] - position;
+        return distance;
+    }
+
+    // Check Distance Covered by Next Ladder
+    public int distance(int[][] ladders, int ladder){
+        int distance = ladders[ladder+1][0] - ladders[ladder][1];
+        return distance;
+    }
+
+    // Check if Space Has Snake
+    public boolean Snake(int[][] snakes, int position){
+        for(int i = 0; i < snakes.length; i++){
+            if(position == snakes[i][0]){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
+
